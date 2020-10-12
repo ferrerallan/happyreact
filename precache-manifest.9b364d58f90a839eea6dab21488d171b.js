8800eadd5b2f67a874d6443d1ddd3d9dbecaa3e1
Allan <ferrerallan@gmail.com>
2020-10-12 16:05:27 -0300

    Updates

---
+++ precache-manifest.9b364d58f90a839eea6dab21488d171b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa855253c4874b807aea0a215c99cdf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae04ecf772e5384e0637",
+    "url": "/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "e89be2d7906395d9337a",
+    "url": "/static/css/main.2bd62ed3.chunk.css"
+  },
+  {
+    "revision": "ae04ecf772e5384e0637",
+    "url": "/static/js/2.081390aa.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/static/js/2.081390aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89be2d7906395d9337a",
+    "url": "/static/js/main.4775a54f.chunk.js"
+  },
+  {
+    "revision": "0cf9b932256dbaa8c8cc",
+    "url": "/static/js/runtime-main.af29317b.js"
+  },
+  {
+    "revision": "91c8d1af04a8fbdde51d2b16c5471b15",
+    "url": "/static/media/landing.91c8d1af.svg"
+  },
+  {
+    "revision": "1653c64706c95e2e85a07cc755abf8ba",
+    "url": "/static/media/logo.1653c647.svg"
+  },
+  {
+    "revision": "658b83029253fe8e316b8f364db7b424",
+    "url": "/static/media/map-marker.658b8302.svg"
+  }
+]);
