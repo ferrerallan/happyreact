90ddef164e740676238a1b73cf561ecd9f4f26f5
Allan <ferrerallan@gmail.com>
2020-10-12 16:25:11 -0300

    Updates

---
+++ precache-manifest.886848a8ec0f6d54a35911555efa44c7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c607aa6da71acb6369096baccea869f",
+    "url": "/happyreact/index.html"
+  },
+  {
+    "revision": "ae04ecf772e5384e0637",
+    "url": "/happyreact/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "bc0605c49072a8b59f77",
+    "url": "/happyreact/static/css/main.5b718948.chunk.css"
+  },
+  {
+    "revision": "ae04ecf772e5384e0637",
+    "url": "/happyreact/static/js/2.081390aa.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/happyreact/static/js/2.081390aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc0605c49072a8b59f77",
+    "url": "/happyreact/static/js/main.4775a54f.chunk.js"
+  },
+  {
+    "revision": "905536121dd0d7e80d00",
+    "url": "/happyreact/static/js/runtime-main.65414943.js"
+  },
+  {
+    "revision": "91c8d1af04a8fbdde51d2b16c5471b15",
+    "url": "/happyreact/static/media/landing.91c8d1af.svg"
+  },
+  {
+    "revision": "1653c64706c95e2e85a07cc755abf8ba",
+    "url": "/happyreact/static/media/logo.1653c647.svg"
+  },
+  {
+    "revision": "658b83029253fe8e316b8f364db7b424",
+    "url": "/happyreact/static/media/map-marker.658b8302.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b364d58f90a839eea6dab21488d171b.js"
+  "/happyreact/precache-manifest.886848a8ec0f6d54a35911555efa44c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/happyreact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
