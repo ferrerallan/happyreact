abf69ce9c5eb296834609e35fcb7f43f45ca562a
Dti Fontes <dti.fontes@gmail.com>
2020-10-14 17:29:53 -0300

    avancando backend

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import mapMarkerImg from '../images/map-marker.svg';
 import {Link} from 'react-router-dom';
-import {FiPlus} from 'react-icons/fi';
-import {Map, TileLayer} from 'react-leaflet';
+import {FiArrowRight, FiPlus} from 'react-icons/fi';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import Leaflet from 'leaflet';
+
+
+const mapIcon = Leaflet.icon({
+    iconUrl : mapMarkerImg,
+    iconSize: [58,68],
+    iconAnchor: [29,68],
+    popupAnchor:[170,2]
+});
 
 function OrphanagesMap(){
     return (
@@ -26,10 +35,20 @@ function OrphanagesMap(){
                 >
                     {/*<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />*/}
                     <TileLayer url= {`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+                    <Marker position={[-22.2385389,-49.9059505]}
+                            icon={mapIcon}
+                    >
+                        <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
+                            Lar doce lar
+                            <Link to="/orphanages/1">
+                                <FiArrowRight size={20} color="FFF"/>
+                            </Link>
+                        </Popup>
+                    </Marker>
             </Map>
             
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF"/>
             </Link>
 
