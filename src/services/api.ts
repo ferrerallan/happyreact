d1cf54e753893ee441843216285ea180ffc8f4f0
Allan <ferrerallan@gmail.com>
2020-10-18 22:45:03 -0300

    visao street

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+
+
 const api = axios.create({
-    baseURL: 'https://ambackend-com-br.umbler.net/'
+    //baseURL: 'https://ambackend-com-br.umbler.net/'
+    baseURL:'http://localhost:3000/'
 });
 
 export default api;
