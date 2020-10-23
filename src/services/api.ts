0213e32a49c905b6dea9bd9be4198aed6d099fb9
Allan <ferrerallan@gmail.com>
2020-10-23 13:37:44 -0300

    base correta

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    //baseURL: 'https://ambackend-com-br.umbler.net/'
-    baseURL:'http://localhost:3000/'
+    baseURL: 'https://ambackend-com-br.umbler.net/'
+    //baseURL:'http://localhost:3000/'
 });
 
 export default api;
